7b824880f631e9735b40eb86467b48c2b26fbb7d
Eduardo Rivero <eduardorivero72005@gmail.com>
2025-03-31 22:55:44 -0400

    Working Jump Animation
    New Jumping Animation made by me. No more free jumping animation! Some bugs and errors but I wanted to push this before any more progress is made as it makes the game feel more alive.

--- Assets/Castaway/animationStateController.cs
+++ Assets/Castaway/animationStateController.cs
@@ -19,8 +19,9 @@ public class AnimationStateController : MonoBehaviour
         isRunning = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
 
         //Checks if space bar is being pressed -Ed
-        isJumping = Input.GetKey(KeyCode.Space);
-        
+        isJumping = Input.GetKeyDown(KeyCode.Space);
+        animator.SetTrigger("Castaway Jump"); // To trigger the jump animation -Ed
+
         // Updates the Animator parameter -Ed
         animator.SetBool("isRunning", isRunning);
         animator.SetBool("isJumping", isJumping);
