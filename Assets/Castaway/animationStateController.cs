752162105f15cfbb07723dc9205746e7e6b0746f
Eduardo Rivero <eduardorivero72005@gmail.com>
2025-04-09 01:00:05 -0400

    computer blue screened
    just trying to fix the conflicting files ignore this

--- Assets/Castaway/animationStateController.cs
+++ Assets/Castaway/animationStateController.cs
@@ -1,12 +1,17 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimationStateController : MonoBehaviour
 {
     private Animator animator;
-    private bool isRunning; 
+    private bool isRunning;
     private bool isJumping;
+    public bool isThrowingRod;
+    private bool isIdle;
+
+    private bool hasCastRod = false;
+    private bool isResettingFromCast = false;
+    private bool canCast = true;
 
     void Start()
     {
@@ -15,15 +20,84 @@ public class AnimationStateController : MonoBehaviour
 
     void Update()
     {
-        //Checks if any of the movement keys are being pressed -Ed
+        // Movement input
         isRunning = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
 
-        //Checks if space bar is being pressed -Ed
-        isJumping = Input.GetKeyDown(KeyCode.Space);
+        // Jump input
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+        }
 
-        // Updates the Animator parameter -Ed
-        animator.SetBool("isRunning", isRunning);
-        animator.SetBool("isJumping", isJumping);
+        // If we just finished casting, press again to reset
+        if (hasCastRod && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Resetting from cast...");
+            animator.SetTrigger("resetFromCast");
+            hasCastRod = false;
+            isResettingFromCast = true;
+            canCast = false;
+        }
+
+        // Start casting only if not resetting and allowed
+        if (canCast && !hasCastRod && !isResettingFromCast)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Started charging rod...");
+                animator.SetTrigger("startRodCharge");
+                isThrowingRod = true;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                Debug.Log("Released rod cast!");
+                animator.SetTrigger("releaseRodCast");
+                isThrowingRod = false;
+                hasCastRod = true;
+                canCast = false;
+            }
+        }
+
+        // Reset throwing flag if not holding
+        if (!Input.GetMouseButton(0) && !hasCastRod)
+        {
+            isThrowingRod = false;
+        }
+
+        animator.SetBool("isThrowingRod", isThrowingRod);
+
+        // Handle movement/idle
+        isIdle = !isRunning && !isJumping;
+
+        if (isThrowingRod)
+        {
+            animator.SetBool("isThrowingRunning", isRunning);
+            animator.SetBool("isThrowingJumping", isJumping);
+        }
+        else
+        {
+            animator.SetBool("isRunning", isRunning);
+            animator.SetBool("isJumping", isJumping);
+            animator.SetBool("isIdle", isIdle);
+        }
+
+        if (isJumping)
+        {
+            isJumping = false;
+        }
+
+        // ✅ Check if we've returned to Idle so we can cast again
+        if (!canCast && animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            Debug.Log("Back to idle, you can cast again!");
+            FinishReset();
+        }
     }
 
+    public void FinishReset()
+    {
+        isResettingFromCast = false;
+        canCast = true;
+    }
 }
