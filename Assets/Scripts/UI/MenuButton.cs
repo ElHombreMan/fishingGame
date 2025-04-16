1ddad0a31f0f17906937ede20560b578a7290601
Eduardo Rivero <eduardorivero72005@gmail.com>
2025-04-16 16:36:41 -0400

    Unfinished Main Menu

--- Assets/Scripts/UI/MenuButton.cs
+++ Assets/Scripts/UI/MenuButton.cs
@@ -13,13 +13,13 @@ public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        textComponent.color = Color.black;
+        textComponent.color = Color.white;
         imageComponent.enabled = true;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        textComponent.color = Color.white;
+        textComponent.color = Color.black;
         imageComponent.enabled = false;
     }
 }
